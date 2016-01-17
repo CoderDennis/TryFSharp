1805a2bca3989c29b17470f5b6e20630eeb12e5c
CoderDennis <dodava@gmail.com>
2016-01-17 15:26:54 -0600

    Mandelbrot with color

--- TryFSharp/TryFSharp/TryFSharpPage.fs
+++ TryFSharp/TryFSharp/TryFSharpPage.fs
@@ -3,6 +3,7 @@
 open System
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
+open Demo.Numerics
 
 type TryFSharpPage() =
     inherit ContentPage()
@@ -10,17 +11,57 @@ type TryFSharpPage() =
     let _ = base.LoadFromXaml(typeof<TryFSharpPage>)
 
     let image = base.FindByName<Image>("image")
+    let progressBar = base.FindByName<ProgressBar>("progressBar")
 
     let rows = 1000
     let cols = 1000
 
+    let center = Complex(-0.75, 0.0)
+    let width = 2.5
+    let height = 2.5
+
+    let ColToX col = center.Real - width / 2.0 + float(col) * width / float(cols)
+    let RowToY row = center.Imaginary - height / 2.0 + float(row) * height / float(rows)
+
+    let MandelbrotIndex c iterations =
+        let rec RecursiveMandelbrot (z : Complex) countdown =
+            if countdown = 0 then
+                -1
+            elif z.Magnitude >= 2.0 then
+                iterations - countdown
+            else
+                RecursiveMandelbrot (z * z + c) (countdown - 1)
+        RecursiveMandelbrot (Complex()) iterations
+
+    let CalculateAsync() =
+        async {
+            return Bmp.Create rows cols (fun row col ->
+                
+                if (col = 0 && float row % float (rows / 100) = 0.0) then
+                    Device.BeginInvokeOnMainThread(fun _ ->
+                        do progressBar.Progress <- float row / float rows)
+
+                let x = ColToX col
+                let y = RowToY row
+                let c = Complex(x, y)
+                match MandelbrotIndex c 100 with
+                    | -1 -> Color.Black
+                    | index -> Color.FromHsla(float index / 64.0 % 1.0, 1.0, 0.5)
+                )
+        }
+
     member this.OnCalculateButtonClicked(sender : Object, args : EventArgs) =
-        let stream = Bmp.Create rows cols (fun row col ->
-            let red = float row / float rows
-            let blue = float col / float cols
-            Color.FromRgb(red, 0.0, blue)
-            )
-        do image.Source <- ImageSource.FromStream(fun _ -> stream)
+        async {
+            let button = sender :?> Button
+            do button.IsEnabled <- false
+
+            let! token = Async.StartChild(CalculateAsync())
+            let! stream = token
+
+            do image.Source <- ImageSource.FromStream(fun _ -> stream)
+
+            do button.IsEnabled <- true
+        } |> Async.StartImmediate
 
 type App() =
     inherit Application(MainPage = TryFSharpPage())
